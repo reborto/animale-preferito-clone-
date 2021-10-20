f298dfa7184a37cd9f458494e269c18a3ebce50d
reborto <tirritoroberto@gmail.com>
2021-10-20 17:09:45 +0200

    feat: first commit

---
+++ esercizio 2010.js
@@ -0,0 +1,18 @@
+let str = prompt("Inserisci 5 nomi separati da una virgola”, ‘Stefania,Sandro,Maria,Giacomo,Giorgia");
+let arrAge = [32, 21, 47, 12, 78];
+/*prompt deve diventare array con split */ 
+let arrName = str.split(",");
+/*esercizi lezione 20-10*/
+let result= arrName.splice(0, 1, reborto);
+            arrName.pop();
+            arrAge.push(Ettore)
+            /*sostituibile con .splice(4, 1, Ettore)*/
+
+let result= arrAge.splice(0, 1, 22);
+            arrAge.unshift(44)
+
+
+/*array multidimensionale?*/
+let array = [arrName, arrAge];
+
+console.log(array);
